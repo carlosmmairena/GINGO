c0fc52fd26a19a5bc90e68cc494d1f743caee9cd
Carlos Mairena <carlosmiguelmairena@gmail.com>
2020-03-17 01:33:15 -0600

    Primer commit que agrega las clases básicas del proyecto

---
+++ src/logica/Cartones.java
@@ -0,0 +1,17 @@
+/*
+    Esta clase de Cartones es un hilo, y cada cartón contendrá una matriz de 8x10
+*/
+
+package logica;
+
+/**
+ *
+ * @author Dixiana Gómez
+ * @author Carlos Mairena
+ */
+public class Cartones {
+    
+    private byte numCarton;
+    private byte matriz[][];
+    
+}
---
+++ src/logica/Jugador.java
@@ -0,0 +1,20 @@
+/*
+    En esta clase, se tiene una agregación de Cartones, por lo que cartones es una matriz de 8x10
+*/
+
+package logica;
+
+/**
+ *
+ * @author Dixiana Gómez
+ * @author Carlos Mairena
+ */
+public class Jugador {
+    
+    private int cedula;
+    private String nombre;
+    private String direccion;
+    private Cartones carton;
+    private int montoIni;
+    
+}
